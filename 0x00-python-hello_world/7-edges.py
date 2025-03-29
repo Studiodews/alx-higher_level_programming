4edf560cfcfddc6dff2e43333dbacf00c19049c8
Studiodews <67610658+Studiodews@users.noreply.github.com>
2025-03-29 13:24:54 +0100

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 firstW = word[0:3]
 secW = word[7:9]
-midW = word[1:9]
+midW = word[1:8]
 print(f"First 3 letters: {firstW}")
 print(f"Last 2 letters: {secW}")
 print(f"Middle word: {midW}")
